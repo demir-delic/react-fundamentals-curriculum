faf37e243af9b90fdb733eb5fd05b50008591cc0
demir-delic <demir.delic@gmail.com>
2018-02-05 14:54:23 -0700

    Add skeleton of Detail component

---
+++ app/components/Detail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Detail extends React.Component {
+    render() {
+        return (
+            <div className="detail">
+                <h1>Detail Component</h1>
+            </div>
+        )
+    }
+}
