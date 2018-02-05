27543a9d7210dbe0db9ee032536ce0a4aca6118e
demir-delic <demir.delic@gmail.com>
2018-02-05 14:54:23 -0700

    Add skeleton of Detail component

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./Header";
 import Landing from "./Landing";
 import Search from "./Search";
@@ -8,13 +8,13 @@ import Forecast from "./Forecast";
 export default class App extends React.Component {
     render() {
         return (
-            <BrowserRouter>
+            <Router>
                 <div className="app">
                     <Header />
                     <Route exact path="/" component={Landing} />
                     <Route path="/forecast" component={Forecast} />
                 </div>
-            </BrowserRouter>
+            </Router>
         )
     }
 }
